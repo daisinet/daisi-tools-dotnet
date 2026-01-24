a8bb1e4dba6f1ce2db75419d328cb0dc70687102
OriginalCoder <22407691+OriginalCoder@users.noreply.github.com>
2026-01-24 10:52:02 -0600

    Add project files.

---
+++ Math/BasicMathTool.cs
@@ -0,0 +1,81 @@
+﻿using Daisi.SDK.Interfaces.Tools;
+using Daisi.SDK.Models.Tools;
+using MathEvaluation;
+using MathEvaluation.Context;
+using MathEvaluation.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Daisi.Tools.Math
+{
+    public class BasicMathTool : IDaisiTool
+    {
+        const string P_EXPRESSION = "expression";
+        const string P_CULTURE = "culture";
+        const string P_MATHCONTEXT = "math-context";
+
+        public string Name => "Daisi Math";
+
+        public string Description => "Evaluates basic math expressions and returns a single result from the expression";
+
+        public ToolParameter[] Parameters => new ToolParameter[]{
+            new ToolParameter(){ Name = P_EXPRESSION, Description = "This is the math expression that is to be evaluated for a single value", IsRequired = true },
+            new ToolParameter(){ Name = P_CULTURE, Description = "This is culture to use when evaluating the expression. Default is \"en-US\".", IsRequired = false },
+            new ToolParameter(){ Name = P_MATHCONTEXT, Description = "Optional Values: \"basic\"-general numeric math without functions; \"scientific\"-basic math plus supports for all trigonomic functions; \"programming\"-basic math, plus supports programming notation such as floor division (\"//\"), exponentiation (\"**\"), and modulo (\"%\") operations; \"dotnet\"-dotnet math syntax and allows for all dotnet 10 math functions and types. Default is \"basic\".", IsRequired = false }
+        }; 
+
+        public ToolExecutionContext GetExecutionContext(IToolContext toolContext, CancellationToken cancellationToken, params ToolParameter[] parameters)
+        {
+            string executionMessage = $"Evaluating Expression Using Basic Math Tool";
+            var task = Task.Run(() =>
+            {
+                try
+                {
+                    var expressionParam = parameters.GetParameter(P_EXPRESSION);
+                    var cultureParam = parameters.GetParameter(P_CULTURE, false);
+                    var contextParam = parameters.GetParameter(P_MATHCONTEXT, false);
+                    var e = expressionParam!.Values.FirstOrDefault();
+                    var c = cultureParam!.Values.FirstOrDefault() ?? "en-US";
+                    var mc = contextParam!.Values.FirstOrDefault() ?? "basic";
+
+                    MathContext mathContext =
+                        mc.ToLower() switch
+                        {
+                            "scientific" => new ScientificMathContext(),
+                            "dotnet" => new DotNetMathContext(),
+                            _ => new MathContext()
+                        };
+
+                    using var express = new MathExpression(e!, mathContext, new CultureInfo(c));
+
+                    string outputMessage = string.Empty;
+                    express.Evaluating += (object? sender, EvaluatingEventArgs args) =>
+                    {
+                        outputMessage += string.Format("{0}: {1} = {2}; {3}\n",
+                            args.Step,
+                            args.MathString[args.Start..(args.End + 1)],
+                            args.Value,
+                            args.IsCompleted ? " //completed" : string.Empty
+                            );
+                    };
+
+                    var result = express.Evaluate();
+
+                    return new ToolResult() { 
+                        Output = result.ToString(), 
+                        OutputMessage = outputMessage, 
+                        OutputFormat = Daisi.Protos.V1.InferenceOutputFormats.PlainText, 
+                        Success = true 
+                    };
+                }
+                catch (Exception ex)
+                {
+                    return new ToolResult() { Success = false, ErrorMessage = ex.Message };
+                }
+            });
+            return new ToolExecutionContext() { ExecutionMessage = executionMessage, ExecutionTask = task };
+        }
+    }
+}
---
+++ Web/Clients/HttpClientTool.cs
@@ -0,0 +1,102 @@
+﻿using Daisi.SDK.Extensions;
+using Daisi.SDK.Interfaces.Tools;
+using Daisi.SDK.Models.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace Daisi.Tools.Web.Clients
+{
+    public class HttpClientTool : IDaisiTool
+    {
+        const string P_URL = "url";
+        const string P_SUMMARIZE = "summarize";
+        const string P_METHOD = "method";
+        const string P_CONTENT = "content";
+        const string P_MEDIATYPE = "media-type";
+
+        public string Name => "Daisi Web Client";
+
+        public string Description => "Sends an HTTP request to the provided URL in real time. Returns a JSON object with \"headers\" and \"content\" properties in a JSON Encoded format.";
+
+        public ToolParameter[] Parameters => new[]{
+            new ToolParameter() { Name = P_URL, Description = "This is the fully qualified URL to send the request.", IsRequired = true },
+            new ToolParameter() { Name = P_SUMMARIZE, Description = "Options: \"True\" or \"False\". If True, the return content will be a summary on the page's main content. If false, the response will be the raw text returned from the URL. Default is False.", IsRequired = false },
+            new ToolParameter() { Name = P_METHOD, Description = "Options: \"GET\",\"POST\",\"PUT\",\"PATCH\". The HTTP method to use for the request. Default is GET.", IsRequired = false },
+            new ToolParameter() { Name = P_CONTENT, Description = "The content to POST, PUT, or PATCH. Not needed if method is \"GET\".", IsRequired = false },
+            new ToolParameter() { Name = P_MEDIATYPE, Description = "The media type for the Content. Default is \"application/json\".", IsRequired = false }
+
+        };
+
+        public ToolExecutionContext GetExecutionContext(IToolContext toolContext, CancellationToken cancellationToken, params ToolParameter[] parameters)
+        {
+            var pMethod = parameters.GetParameter(P_METHOD, false);
+            var method = pMethod?.Values.FirstOrDefault() ?? "get";
+            method = method.ToLower();
+
+            var pUrl = parameters.GetParameter(P_URL);
+            var url = pUrl.Values.FirstOrDefault();
+            var executionMessage = string.Format($"HTTP {0}: {1}", method, url?.Left(30)); 
+
+            var task = Task.Run(async () =>
+            {
+                try
+                {
+                    var result = new ToolResult();
+
+                    IHttpClientFactory? httpClientFactory = toolContext.Services.GetService<IHttpClientFactory>();
+                    if (httpClientFactory is not null)
+                    {
+       
+
+                        using var client = httpClientFactory.CreateClient();
+
+                        string? outgoingContent = null;
+                        string? mediaType = null;
+                        if (method != "get")
+                        {
+                            var pContent = parameters.GetParameter(P_CONTENT);
+                            outgoingContent = pContent.Values.FirstOrDefault();
+
+                            var pMediaType = parameters.GetParameter(P_MEDIATYPE, false);
+                            mediaType = pMediaType?.Values.FirstOrDefault() ?? "application/json";
+                        }
+
+                        using HttpResponseMessage httpResponse =
+                            method switch
+                            {
+                                "post" => await client.PostAsync(url, new StringContent(outgoingContent!, Encoding.UTF8, mediaType), cancellationToken),
+                                "put" => await client.PutAsync(url, new StringContent(outgoingContent!, Encoding.UTF8, mediaType), cancellationToken),
+                                "patch" => await client.PatchAsync(url, new StringContent(outgoingContent!, Encoding.UTF8, mediaType), cancellationToken),
+                                _ => await client.GetAsync(url, cancellationToken)
+                            };
+
+                        httpResponse.EnsureSuccessStatusCode();
+
+                        string responseBody = await httpResponse.Content.ReadAsStringAsync();
+
+                        result.Output = $"{{\n  \"headers\" : \"{JsonEncodedText.Encode(httpResponse.Headers.ToString())}\",\n  \"content\" : \"{JsonEncodedText.Encode(responseBody)}\"\n}}";
+                        result.Success = true;
+                        result.OutputFormat = Protos.V1.InferenceOutputFormats.Json;
+                    }
+                    else
+                    {
+                        result.ErrorMessage = "HttpClientFactory is not available in the current context.";
+                        result.Success = false;
+                    }
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    return new ToolResult() { Success = false, ErrorMessage = ex.Message };
+                }
+            });
+
+            return new ToolExecutionContext() { ExecutionMessage = executionMessage, ExecutionTask = task };
+        }
+    }
+}
